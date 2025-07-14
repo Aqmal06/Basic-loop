51b1fa78c8214911487a639fa790b8368dba0f70
Muhd Aqmal <muhammadaqmal721@gmail.com>
2025-07-14 22:10:02 +0800

    cpploops
    Basic looping notes

--- looptest.cpp
+++ looptest.cpp
@@ -2,11 +2,13 @@
 
 int main(){
 
-    /*for(int i=1; i<=10; i+=1){
+    //forloop
+    for(int i=1; i<=10; i+=1){
         std::cout<<i<<"\n";
     }
-    std::cout<<"\n"<<"HAPPY BIRTHDAY!";*/
+    std::cout<<"\n"<<"HAPPY BIRTHDAY!";
 
+    //nestedloop
     //think of this is an row
     for(int i = 1; i <= 5; i+=1){
         //while this is a column
