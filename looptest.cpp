06728607f3219bb16637c074abf77b44fe68c192
Muhd Aqmal <muhammadaqmal721@gmail.com>
2025-07-14 21:57:25 +0800

    cpploops
    Basic looping notes

---
+++ looptest.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(){
+
+    /*for(int i=1; i<=10; i+=1){
+        std::cout<<i<<"\n";
+    }
+    std::cout<<"\n"<<"HAPPY BIRTHDAY!";*/
+
+    //think of this is an row
+    for(int i = 1; i <= 5; i+=1){
+        //while this is a column
+        for(int j = 1; j <=i; j+=1){
+            std::cout<<"* ";
+        }
+        std::cout<<"\n";
+    }
+    
+    return 0;
+}
