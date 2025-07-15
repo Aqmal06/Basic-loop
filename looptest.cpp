f4d7a71fa2cb8acc64f9e2b78180d47f11743a44
Muhd Aqmal <muhammadaqmal721@gmail.com>
2025-07-15 23:53:05 +0800

    forloops
    C++ Forloops diary

--- looptest.cpp
+++ looptest.cpp
@@ -4,19 +4,12 @@ int main(){
 
     //forloop
     for(int i=1; i<=10; i+=1){
+        //code will printout how many i value are being iterate.
         std::cout<<i<<"\n";
     }
+    //this code is out of the loop body.
+    //by the end of the i value, it will be printout one time.
     std::cout<<"\n"<<"HAPPY BIRTHDAY!";
 
-    //nestedloop
-    //think of this is an row
-    for(int i = 1; i <= 5; i+=1){
-        //while this is a column
-        for(int j = 1; j <=i; j+=1){
-            std::cout<<"* ";
-        }
-        std::cout<<"\n";
-    }
-    
     return 0;
 }
